f9378a12d758a48af2bb047dd8b3ff42e58ad86e
DDEVADO1 <ddevado1@ford.com>
2019-08-13 14:57:48 +0530

    formated test case

--- src/test/java/field/mapper/FieldMapperTest.java
+++ src/test/java/field/mapper/FieldMapperTest.java
@@ -13,42 +13,44 @@ import field.mapper.util.FieldMapperUtility;
  */
 public class FieldMapperTest {
 
-    @Test
-    public void test_copyFromOneObjectToAnotherObject_withValue() {
-        FromClass fromClass = new FromClass("10");
-        ToClass toClass = new ToClass();
-        FieldMapperUtility.copy(fromClass, toClass);
-        Assert.assertEquals(10, toClass.getNumber().intValue());
-    }
+	@Test
+	public void test_copyFromOneObjectToAnotherObject_withValue() {
+		FromClass fromClass = new FromClass("10");
+		ToClass toClass = new ToClass();
+		FieldMapperUtility.copy(fromClass, toClass);
+		Assert.assertEquals(10, toClass.getNumber().intValue());
+	}
 
-    @Test
-    public void test_copyFromOneObjectToAnotherObject_withoutValue() {
-        FromClass fromClass = new FromClass();
-        ToClass toClass = new ToClass();
-        FieldMapperUtility.copy(fromClass, toClass);
-        Assert.assertEquals(null, toClass.getNumber());
-    }
+	@Test
+	public void test_copyFromOneObjectToAnotherObject_withoutValue() {
+		FromClass fromClass = new FromClass();
+		ToClass toClass = new ToClass();
+		FieldMapperUtility.copy(fromClass, toClass);
+		Assert.assertEquals(null, toClass.getNumber());
+	}
 
-    @Test
-    public void test_copyFromOneObjectToAnotherClass_withValues() {
-        FromClass fromClass = new FromClass("10");
-        ToClass toClass = FieldMapperUtility.copy(fromClass, ToClass.class);
-        Assert.assertEquals(10, toClass.getNumber().intValue());
-    }
+	@Test
+	public void test_copyFromOneObjectToAnotherClass_withValues() {
+		FromClass fromClass = new FromClass("10");
+		ToClass toClass = FieldMapperUtility.copy(fromClass, ToClass.class);
+		Assert.assertEquals(10, toClass.getNumber().intValue());
+	}
 
-    @Test
-    public void test_copyFromOneObjectToAnotherClass_withoutValues() {
-        FromClass fromClass = new FromClass();
-        ToClass toClass = FieldMapperUtility.copy(fromClass, ToClass.class);
-        Assert.assertEquals(null, toClass.getNumber());
-    }
+	@Test
+	public void test_copyFromOneObjectToAnotherClass_withoutValues() {
+		FromClass fromClass = new FromClass();
+		ToClass toClass = FieldMapperUtility.copy(fromClass, ToClass.class);
+		Assert.assertEquals(null, toClass.getNumber());
+	}
 
-    @Test(expected = RuntimeException.class)
-    public void test_methodNotAvailable_throwException() {
-        InvalidConversionMethod fromClass= new InvalidConversionMethod("test");
-        InvalidConversionMethod c2 = FieldMapperUtility.copy(fromClass, InvalidConversionMethod.class);
+	/**
+	 * 
+	 */
+	@Test(expected = RuntimeException.class)
+	public void test_methodNotAvailable_throwException() {
+		InvalidConversionMethod fromClass = new InvalidConversionMethod("test");
+		FieldMapperUtility.copy(fromClass, InvalidConversionMethod.class);
 
-    }
-
+	}
 
 }
